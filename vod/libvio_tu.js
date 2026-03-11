44438b06196e98f23955701df15ea16facfa0bec
奈非天 <269933171@qq.com>
2026-03-11 22:59:02 +0800

    更新libvio域名

--- vod/libvio_tu.js
+++ vod/libvio_tu.js
@@ -2,7 +2,7 @@
 const cheerio = createCheerio()
 
 let $config = argsify($config_str)
-const SITE = $config.site || "https://www.libvio.cc"
+const SITE = $config.site || "https://www.libvio.in"
 const UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
 const headers = {
     'Referer': `${SITE}/`,
